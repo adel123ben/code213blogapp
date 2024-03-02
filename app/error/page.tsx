f58e715192cb5c8684564f7327e8cf4b10d82363
adel123ben <adelformat541@gmail.com>
2024-03-02 20:25:05 +0100

    update

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,18 +1,11 @@
-// pages/_error.tsx
 import React from 'react'
 
-const ErrorPage: React.FC<{ statusCode: number }> = ({ statusCode }) => {
+function Error() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-200">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold text-red-500 mb-4">{statusCode}</h1>
-        <p className="text-lg font-semibold">Oops! Something went wrong.</p>
-        <p className="text-lg mt-2">Please try again later.</p>
-      </div>
+    <div>
+    Error went rowg
     </div>
   )
 }
 
-
-
-export default ErrorPage
+export default Error
